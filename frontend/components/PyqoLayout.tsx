b0b454f5c37ac30c11f340fecbb01c8725aa7883
Ryan Downing <ryansdowning1@gmail.com>
2024-11-10 13:43:40 -0500

    add logout button

--- frontend/components/PyqoLayout.tsx
+++ frontend/components/PyqoLayout.tsx
@@ -1,11 +1,15 @@
 import { PropsWithChildren } from "react";
 
+import { useLocalStorage } from "usehooks-ts";
+
 import { AppShell, Burger, Button, Group, Stack } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { IconLogout } from "@tabler/icons-react";
 
 import AuthenticatedProvider from "./AuthenticatedProvider";
 
 export function PyqoLayout({ children }: PropsWithChildren) {
+  const [, setToken] = useLocalStorage("token", "");
   const [opened, { toggle }] = useDisclosure();
 
   return (
@@ -30,32 +34,43 @@ export function PyqoLayout({ children }: PropsWithChildren) {
           </Group>
         </AppShell.Header>
         <AppShell.Navbar pt="sm">
-          <Stack gap="xs">
-            {Array(5)
-              .fill(0)
-              .map((_, index) => (
-                <Button
-                  key={index}
-                  h={28}
-                  variant="transparent"
-                  w="100%"
-                  justify="flex-start"
-                  styles={(theme) => ({
-                    root: {
-                      "&:hover": {
-                        backgroundColor: theme.colors.gray[1],
-                        color: theme.colors.blue[7],
+          <Stack justify="space-between" h="100%">
+            <Stack gap="xs" w="100%">
+              {Array(5)
+                .fill(0)
+                .map((_, index) => (
+                  <Button
+                    key={index}
+                    h={28}
+                    variant="transparent"
+                    w="100%"
+                    justify="flex-start"
+                    styles={(theme) => ({
+                      root: {
+                        "&:hover": {
+                          backgroundColor: theme.colors.gray[1],
+                          color: theme.colors.blue[7],
+                        },
+                        "&:active": {
+                          backgroundColor: theme.colors.blue[0],
+                          color: theme.colors.blue[9],
+                        },
                       },
-                      "&:active": {
-                        backgroundColor: theme.colors.blue[0],
-                        color: theme.colors.blue[9],
-                      },
-                    },
-                  })}
-                >
-                  Button {index + 1}
-                </Button>
-              ))}
+                    })}
+                  >
+                    Button {index + 1}
+                  </Button>
+                ))}
+            </Stack>
+            <Button
+              variant="transparent"
+              justify="flex-start"
+              w="100%"
+              onClick={() => setToken("")}
+              leftSection={<IconLogout />}
+            >
+              Logout
+            </Button>
           </Stack>
         </AppShell.Navbar>
         <AppShell.Main>{children}</AppShell.Main>
